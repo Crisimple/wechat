9ef19f272c73c550e072102e9a1bc04e435574c0
Crisimple <1240281595@qq.com>
2019-07-25 07:41:18 +0800

    rebulid django environment

--- mypublic/mypublic/wsgi.py
+++ mypublic/mypublic/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for WechatPublic project.
+WSGI config for mypublic project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WechatPublic.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mypublic.settings')
 
 application = get_wsgi_application()
