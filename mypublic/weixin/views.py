613db67f7ae556c08748b204c1cb014a6eb189f2
Crisimple <1240281595@qq.com>
2019-07-30 07:56:06 +0800

    add requirements.txt

--- mypublic/weixin/views.py
+++ mypublic/weixin/views.py
@@ -1,5 +1,17 @@
+import hashlib
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.template import RequestContext, Template
+from django.utils.encoding import smart_str, smart_bytes
 from django.shortcuts import render
+from xml.etree import ElementTree as etree
+
 
 # Create your views here.
 def weixin(request):
     pass
+
+
+if __name__ == "__main__":
+    pass
+
