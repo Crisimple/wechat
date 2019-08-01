3a29ec67261921f69d8df3b961f904cf011964c5
crisimple <crisimple@foxmail.com>
2019-08-01 16:09:22 +0800

    add check_signature to chat with WeixinServer

--- mypublic/views.py
+++ mypublic/views.py
@@ -4,39 +4,41 @@ from django.views.decorators.csrf import csrf_exempt
 
 import hashlib
 import xml.etree.ElementTree as ET
+import time
 
 
 # Create your views here.
 # django默认开启了csrf防护，@csrf_exempt是去掉防护
+# 和微信服务器进行参数交互
 @csrf_exempt
-def weixin(request):
+def check_signature(request):
     if request.method == "GET":
         print("request: ", request)
         # 接受微信服务器get请求发过来的参数
-        signature = request.GET.get('signature', None)
-        timestamp = request.GET.get('timestamp', None)
-        nonce = request.GET.get('nonce', None)
-        echostr = request.GET.get('echostr', None)
+        signature = request.GET.get('signature', '')
+        timestamp = request.GET.get('timestamp', '')
+        nonce = request.GET.get('nonce', '')
+        echostr = request.GET.get('echostr', '')
         # 微信公众号处配置的token
         token = str("Txy159wx")
+
         # 将参数梵高list中排序合成字符串，再用sha1加密得到新的字符串与微信发过来的signature对比，如果相同就返回echostr给服务器，校验通过
         # ISSUES: TypeError: '<' not supported between instances of 'NoneType' and 'str'
         hashlist = [token, timestamp, nonce]
         hashlist.sort()
+        print("[token, timestamp, nonce]: ", hashlist)
+
         hashstr = ''.join([s for s in hashlist]).encode('utf-8')
+        print('hashstr before sha1', hashstr)
+
         hashstr = hashlib.sha1(hashstr).hexdigest()
+        print('hashstr sha1', hashstr)
+
         if hashstr == signature:
             return HttpResponse(echostr)
         else:
             return HttpResponse("false")
     else:
-        otherContent = autoreply(request)
-        return HttpResponse(otherContent)
+        return HttpResponse('success')
 
 
-# 自动回复
-def autoreply(request):
-    try:
-        pass
-    except Exception as e:
-        return e
